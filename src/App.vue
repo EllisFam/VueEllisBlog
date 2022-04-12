0cb3a2b09f6c8e69f348ba6707dc8c7172c76829
Courtney Ellis <courtneycaloca@gmail.com>
2022-04-12 17:03:25 -0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,108 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+      <router-link class="navbar-brand" to="/">The Ellis'</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/gallery">Gallery</router-link>
+          </li>
+          <li>
+            <router-link class="nav-link" to="/blog">Blog</router-link>
+          </li>
+          <li>
+            <router-link class="nav-link" to="/about">About</router-link>
+          </li>
+        </ul>
+      </div>
     </nav>
     <router-view />
+    <div class="footer">
+      <div class="footer-element">
+        <span>&copy; 2022</span>
+      </div>
+      <div class="footer-element">
+        <span>Caden and Courtney Ellis</span>
+      </div>
+      <div class="footer-element">
+        <span><a href="https://github.com/EllisFam">GitHub Link</a></span>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.page {
+  background-color: whitesmoke;
+}
+.page-header {
   text-align: center;
-  color: #2c3e50;
+}
+/* Navbar */
+.navbar-brand {
+  font-family: "Pacifico", cursive !important;
+  color: #f5f5f5 !important;
+  font-size: 30px !important;
+}
+.bg-dark {
+  background-color: #222 !important;
+}
+.navbar-nav {
+  display: flex !important;
+  justify-content: space-evenly !important;
+  overflow: hidden;
+  flex-wrap: wrap;
+  width: 100%;
+  padding: 0px 0px 0px 0px;
+}
+.nav-item {
+  display: flex !important;
+  justify-content: space-evenly !important;
+}
+.nav-link {
+  color: whitesmoke !important;
+  font-family: Helvetica, Arial, sans-serif !important;
+  overflow: hidden;
+  font-size: 20px;
+}
+/* Footer */
+.footer {
+  display: flex;
+  justify-content: space-evenly;
+  background-color: #222;
+  align-items: center;
+  margin-top: auto;
+  width: 100%;
 }
 
-nav {
-  padding: 30px;
+.footer-element {
+  display: flex;
+  align-items: center;
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.footer span {
+  font-family: Helvetica, Arial, sans-serif !important;
+  font-size: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  color: whitesmoke;
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.footer span a {
+  text-decoration: none;
+  color: whitesmoke;
+}
+.footer span a:hover {
+  text-decoration: none;
+  color: whitesmoke;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,107 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="flex-wrapper">
+      <div class="hero">
+        <h1>The Ellis'</h1>
+      </div>
+      <div class="about">
+        <h3>About Us</h3>
+        <div class="container">
+          <div class="row">
+            <div class="col-md">
+              <p>
+                Hello! We are the Ellis Family. Caden grew up in Abilene, Texas
+                and went on a mission to Buenos Aires, Argentina from 2016-2018.
+                Courtney was raised in Pleasant Grove, Utah. We met each other
+                at BYU in the fall of 2018 during both of our first semesters at
+                BYU. Eventually we started dating in the spring of 2019. After a
+                busy summer of traveling and getting to know each other more
+                (mostly through long distance) we got engaged in August and
+                married on November 23, 2019.
+              </p>
+            </div>
+            <hr />
+            <div class="col-md">
+              <p>
+                We love to play games and watch shows together! If you ever want
+                to play games with us just ask! We also enjoy making good food
+                together and going to Disneyland. We love both the Marvel and
+                Star Wars universes. Caden has enjoyed introducing Courtney to
+                these, and more, during their relationship.
+              </p>
+            </div>
+          </div>
+          <hr />
+          <div class="row">
+            <div class="col-md">
+              <p>
+                Caden studied Computer Engineering at BYU and loves to learn new
+                things. He is currently working at SchedMD in Lehi, Utah
+                developing the
+                <a href="https://www.schedmd.com/">Slurm</a> workload manager.
+              </p>
+            </div>
+            <div class="col-md">
+              <p>
+                Courtney is a fine specimen to behold. She is breath taking and
+                frankly Caden is lucky she even gave him a chance. She
+                <strong>loves</strong> ice cream. She is currently working at
+                <a href="https://www.eidebailly.com/">Eide Bailly</a> as a
+                Salesforce Developer and Consultant.
+              </p>
+            </div>
+          </div>
+        </div>
+        <hr />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
+
+<style scoped>
+/* Home Page */
+.hero {
+  /* background-image: url(../img/IMG_5287.jpg); */
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 400px;
+}
+h1 {
+  font-family: "Pacifico", cursive !important;
+}
+.hero h1 {
+  text-align: center;
+  padding-top: 30px;
+  font-size: 150px;
+  color: rgba(255, 255, 255, 0.479);
+}
+.about {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+.about h3 {
+  text-align: center;
+  padding: 18px 50px 10px 50px;
+}
+.about p {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 15px;
+  margin: 0px 50px 10px 50px;
+}
+.about p a {
+  text-decoration: none;
+  color: gray;
+}
+.about p a:hover {
+  text-decoration: none;
+  color: blue;
+}
+</style>
